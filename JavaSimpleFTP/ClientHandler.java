e722570de8d3aa3facffb24a4f551120f2748e74
Dustin Ames Pfeiffer <dpfeiffe@be-linux19.lt.ucsc.edu>
2015-02-08 18:09:56 -0800

    Java Handler near completion

--- JavaSimpleFTP/ClientHandler.java
+++ JavaSimpleFTP/ClientHandler.java
@@ -3,12 +3,60 @@ import java.io.*;
 
 public class ClientHandler implements Runnable
 {
+	
     class GetHandler implements Runnable
     {
+        InetAddress addr;
+        int portNum;
+        String file;
         
+        public GetHandler(InetAddress addr, int portNum, String file){
+        	this.addr = addr;
+        	this.portNum = portNum;
+        	this.file = file;
+        }
+        
+        public void run(){
+        	
+        	int d;
+        	PrintWriter out = null;
+        	FileInputStream in = null;
+        	try {
+        		Socket sock = new Socket(addr, portNum);
+            	out = 
+            			new PrintWriter(sock.getOutputStream(), true);
+            	in = new FileInputStream(file);
+            	while ((d = in.read()) != -1){
+            		out.write(d);
+            	}
+        	} 
+        	catch (Exception e){ 
+        		System.out.println("This is mean");
+        	}
+        		finally {
+        	
+        		if (in != null){
+        			try{
+        				in.close();
+        			}
+        			catch (Exception e){
+        				System.out.println("WHY!?");
+        			}
+        		}
+        		if (out != null){
+        			try{
+        				out.close();
+        			}
+        			catch (Exception e){
+        				System.out.println("WHY!?");
+        			}
+        		}
+        	}
+        }
     }
     
     Socket client;
+    int port;
 	public ClientHandler (Socket c)
 	{
         client = c;
@@ -40,8 +88,21 @@ public class ClientHandler implements Runnable
         String[] words = line.split (" ");
         switch (words[0])
         {
-            case "exit": client.close(); break;
-            case "port": 
+            case "exit": try { client.close(); }
+            			 catch (Exception e) {
+            				System.out.println("No");
+            			 }
+            			 break;
+            case "get":
+            	(new Thread(new GetHandler(client.getInetAddress(), port, words[1]))).start();
+            	break;
+            case "port":
+            	handlePort(words);
+            	break;
         }
+    }    
+    
+    public void handlePort (String[] atgs){
+    	port = Integer.parseInt(atgs[1]);
     }
 }
